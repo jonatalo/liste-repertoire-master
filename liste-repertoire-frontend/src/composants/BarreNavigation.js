b0d534172a4eed36518f2c8ed111744d35f8c49b
Mathieu Robin <mt98robin@gmail.com>
2021-01-13 14:28:25 -0500

    Ajout des path sur aap.js

--- liste-repertoire-frontend/src/composants/BarreNavigation.js
+++ liste-repertoire-frontend/src/composants/BarreNavigation.js
@@ -17,7 +17,13 @@ function BarreNavigation() {
                     </LinkContainer>
                     <LinkContainer to="/admin">
                         <Nav.Link>Admin</Nav.Link>
-                    </LinkContainer>            
+                    </LinkContainer>
+                    <LinkContainer to="/prive">
+                        <Nav.Link>Privé</Nav.Link>
+                    </LinkContainer>  
+                    <LinkContainer to="/formulaire">
+                        <Nav.Link>Formulaire</Nav.Link>
+                    </LinkContainer>              
                 </Nav>
             </Navbar.Collapse>
         </Navbar>
