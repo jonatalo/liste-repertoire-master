aff3b31304d61cea649ddadd6c81664fb9dd11e2
jonatalo <jonathanlangelier007@gmail.com>
2021-01-20 14:19:32 -0500

    Mise a jour du formulaire connection client

--- liste-repertoire-frontend/src/pages/PageFormulaireConnection.js
+++ liste-repertoire-frontend/src/pages/PageFormulaireConnection.js
@@ -8,7 +8,14 @@ import Button from 'react-bootstrap/Button';
 import FormulaireConnection from '../composants/FormulaireConnection'
 function PageFormulaireConnection()
 {
-    <>
-        < FormulaireConnection />
-    </>
+    return (
+        <>
+            < FormulaireConnection />
+            <Link to="/NouveauUtilisateur">
+                    <Button>Nouvelle Utilisateur</Button>    
+                </Link>
+        </>
+    );
 }
+
+export default PageFormulaireConnection;
