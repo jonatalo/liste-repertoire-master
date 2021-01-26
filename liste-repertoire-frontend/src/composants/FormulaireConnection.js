70b63b9aa85339372c8d63dee530380c9e5553b5
Etienne <1994821@csfoy.ca>
2021-01-26 17:00:40 -0500

    Verification connection bug pour admin

--- liste-repertoire-frontend/src/composants/FormulaireConnection.js
+++ liste-repertoire-frontend/src/composants/FormulaireConnection.js
@@ -7,22 +7,45 @@ import { Form } from 'react-bootstrap';
 import Button from 'react-bootstrap/Button';
 import {UtiliseAuth} from '../context/auth'
 import Alert from 'react-bootstrap/Alert';
+import { Redirect } from 'react-router-dom';
 
 function FormulaireConnection() {
     const [nomUtilisateur, setNomUtilisateur] = useState('');
     const [motPasse, setMotPasse] = useState('');
-    const {setAuthentification} = UtiliseAuth();
+    const {authentification,setAuthentification} = UtiliseAuth();
+    const [rediriger, setRediriger] = useState(false);
 
     const connectionUtilisateur = async () => {
-        const utilisateurjson = await fetch(`/api/utilisateur/${nomUtilisateur}`);
+        const utilisateurjson = await fetch(`/api/utilisateurs/${nomUtilisateur}`);
         const utilisateur = await utilisateurjson.json();
         
         if(motPasse !== utilisateur.motDePasse){
             <Alert variant="Danger" >Mot de passe invalide!</Alert>
         }
-    };
+        else{
+            if(nomUtilisateur=='admin'){
+                setAuthentification(2);
+            }
+            else{
+                setAuthentification(1);
+            }
+        }
+        setRediriger(true);
+    }
+    function afficherRedirection() {
+        
+        if (rediriger === true) {
+            if (authentification == 1) {
+                return <Redirect to="/demande-speciale" />
+            }
+            else if (authentification == 2) {
+                return <Redirect to="/admin" />
+            }
+        }
+    }
 return(
     <>
+    {afficherRedirection()}
         <Form>
             <Form.Group> 
                 <Form.Control type="text"  class="fadeIn second" name="login" placeholder="login"
