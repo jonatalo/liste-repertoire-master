b9d8fe6c84143c8b9608141074924a6e039120d6
jonatalo <jonathanlangelier007@gmail.com>
2021-02-02 16:00:02 -0500

    Mettre a jour la fonction trie

--- liste-repertoire-frontend/src/composants/ListePieceTest.js
+++ liste-repertoire-frontend/src/composants/ListePieceTest.js
@@ -8,15 +8,16 @@ import Button from 'react-bootstrap/Button';
 import { Table } from 'react-bootstrap';
 
 
+
 function ListePieceTest({ pieces }) {
-    const [CategorieTrie,setCategorieTrie]=useState("Croissant");
+    const [CategorieTrie,setCategorieTrie]=useState("Rien");
     const [PieceTrie,setPieceTrie]=useState("Rien");
     const [NomArtisteTrie,setNomArtisteTrie]=useState("Rien");
 
     if (pieces?.length) {
         var dictionnaireCategories = Object();
-        var dictionnairePiece = Object();
-        var dictionnaireArtiste = Object();
+        var dictionnairePieces = Object();
+        var dictionnaireArtistes = Object();
         pieces.forEach(piece => {
             piece.categories.forEach(categorie => {
                 if (dictionnaireCategories[categorie] === undefined) {
@@ -24,9 +25,23 @@ function ListePieceTest({ pieces }) {
                 }
             })            
         });
-       
+        pieces.forEach(piece => {
+            
+                if (dictionnaireArtistes[piece.artiste] === undefined) {
+                    dictionnaireArtistes[piece.artiste] = true;
+                }
+                        
+        });
+        pieces.forEach(piece => {
+            
+            if (dictionnairePieces[piece.titre] === undefined) {
+                dictionnairePieces[piece.titre] = true;
+            }
+                    
+    });
 
-
+        var artistes = Object.keys(dictionnaireArtistes);
+        var titres = Object.keys(dictionnairePieces);
         var categories = Object.keys(dictionnaireCategories);
         //ne pas oublier de modifier les path dans le server
         if(CategorieTrie == "Croissant")
@@ -34,6 +49,7 @@ function ListePieceTest({ pieces }) {
             categories = categories.sort((categorie1, categorie2) => 
             categorie1.toLowerCase().localeCompare(categorie2.toLowerCase()) 
             );// sort une function lamda
+
         }
         else if (CategorieTrie == "Decroissant")
         {
@@ -42,18 +58,22 @@ function ListePieceTest({ pieces }) {
             );// sort une function lamda
 
         }
+
+
+
+        
         return (
             <>
                 <Table class="table table-bordered">
                     <thead>
                         <tr>
                             <th>Titre
-                                <Button variant="success" className="m-1" size="sm" >&uarr;</Button>
-                                <Button variant="success" className="m-1" size="sm" >&darr;</Button>
+                                <Button variant="success" className="m-1" size="sm" onClick={() => setPieceTrie("Croissant")} >&uarr;</Button>
+                                <Button variant="success" className="m-1" size="sm" onClick={() => setPieceTrie("Decroissant")} >&darr;</Button>
                             </th>
                             <th>Artiste
-                                <Button variant="success" className="m-1" size="sm" >&uarr;</Button>
-                                <Button variant="success" className="m-1" size="sm" >&darr;</Button>
+                                <Button variant="success" className="m-1" size="sm" onClick={() => setNomArtisteTrie("Croissant")}>&uarr;</Button>
+                                <Button variant="success" className="m-1" size="sm" onClick={() => setNomArtisteTrie("Decroissant")} >&darr;</Button>
                             </th>
                             <th>Categorie
                             <Button variant="success" className="m-1" size="sm" onClick={() => setCategorieTrie("Croissant")} >&uarr;</Button>
@@ -63,9 +83,38 @@ function ListePieceTest({ pieces }) {
                     </thead>
                     <tbody>
                         {categories.map((categorie) => {
-                        const piecesAssociees = pieces.filter((piece) => 
+                        var piecesAssociees = pieces.filter((piece) => 
                             piece.categories.indexOf(categorie) !== -1);
-                            
+                        if(NomArtisteTrie == "Croissant")
+                        {
+                            piecesAssociees =piecesAssociees.sort((categorie1, categorie2) => 
+                                categorie1.artiste.toLowerCase().localeCompare(categorie2.artiste.toLowerCase()) 
+                                );// sort une function lamda
+                           
+                        }
+                        else if (NomArtisteTrie == "Decroissant")
+                        {
+                            piecesAssociees = piecesAssociees.sort((categorie1, categorie2) => 
+                                categorie1.artiste.toLowerCase().localeCompare(categorie2.artiste.toLowerCase()) * -1 
+                                );// sort une function lamda
+                             
+                        }
+
+                            if(PieceTrie == "Croissant")
+                            {
+                                piecesAssociees =piecesAssociees.sort((categorie1, categorie2) => 
+                                categorie1.titre.toLowerCase().localeCompare(categorie2.titre.toLowerCase()) 
+                                );// sort une function lamda
+                        
+                            }
+                            else if (PieceTrie == "Decroissant")
+                            {
+                                piecesAssociees = piecesAssociees.sort((categorie1, categorie2) => 
+                                categorie1.titre.toLowerCase().localeCompare(categorie2.titre.toLowerCase()) * -1 
+                                );// sort une function lamda
+                        
+                            }
+                        
                         return (
                             <>
                                 {
