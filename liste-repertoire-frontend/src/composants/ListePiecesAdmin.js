968462079febb4ba833016b245b348a3c0555399
jonatalo <jonathanlangelier007@gmail.com>
2021-02-04 20:06:20 -0500

    Ajoute du trie dans la liste piece Admin

--- liste-repertoire-frontend/src/composants/ListePiecesAdmin.js
+++ liste-repertoire-frontend/src/composants/ListePiecesAdmin.js
@@ -1,47 +1,194 @@
-import React from 'react';
+import {
+    React,
+    useState
+} from 'react'
 import Alert from 'react-bootstrap/Alert'
 import Button from 'react-bootstrap/Button';
 import { Link } from 'react-router-dom';
+import { Table } from 'react-bootstrap';
+import {FaAngleUp } from "react-icons/fa";
+import {FaAngleDown } from "react-icons/fa";
+function DiviserParCategorie(pieceMusicales){
+    const pieceConstruteur = {_id:"", tite:"",artiste:"",categories:""};
+        var pieceMusicaleCategorie=[];
+        pieceMusicales.forEach(piece =>{
+            
+ 
+            if(piece.categories.length == 1)
+            {
+                var pieceMusical =Object.create(pieceConstruteur);
+                pieceMusical._id =piece._id;
+                pieceMusical.titre = piece.titre;
+                pieceMusical.artiste = piece.artiste;
+                pieceMusical.categories=piece.categories[0];
+                pieceMusicaleCategorie.push(pieceMusical);
+            }
+            else if(piece.categories.length > 1)
+            {
+               
+                var categories=[];
+                
+                piece.categories.forEach(categorie=>{
+                    categories.push(categorie);
+                    
+                });
+                for (let index = 0; index < categories.length; index++) {
+                    var pieceMusical =Object.create(pieceConstruteur);
+                    pieceMusical._id =piece._id;
+                    pieceMusical.titre = piece.titre;
+                    pieceMusical.artiste = piece.artiste;                   
+                    pieceMusical.categories=categories[index];                
+                    pieceMusicaleCategorie.push(pieceMusical);
+                }
+            }
+        })
+        return pieceMusicaleCategorie;
+
+}
 
 function ListePiecesAdmin({ pieces }) {
-    if (pieces?.length) {
-        var dictionnaireCategories = Object();
+    const [CategorieTrie,setCategorieTrie]=useState("Rien");
+    const [PieceTrie,setPieceTrie]=useState("Rien");
+    const [NomArtisteTrie,setNomArtisteTrie]=useState("Rien");
 
-        pieces.forEach(piece => {
-            piece.categories.forEach(categorie => {
-                if (dictionnaireCategories[categorie] === undefined) {
-                    dictionnaireCategories[categorie] = true;
-                }
-            })            
-        });
 
-        var categories = Object.keys(dictionnaireCategories);
-
+     if (pieces?.length) {
+        var pieceMusicales=pieces;
+        if(CategorieTrie == "Croissant"){
+            var pieceMusicales=pieces;
+            pieceMusicales = DiviserParCategorie(pieceMusicales);            
+            pieceMusicales = pieceMusicales.sort((pieceA, pieceB) => 
+                pieceA.categories.toLowerCase().localeCompare(pieceB.categories.toLowerCase())
+            );
+            
+         }
+         else if (CategorieTrie == "Decroissant"){
+            var pieceMusicales=pieces;
+            pieceMusicales = DiviserParCategorie(pieceMusicales);            
+            pieceMusicales = pieceMusicales.sort((pieceA, pieceB) => 
+                pieceA.categories.toLowerCase().localeCompare(pieceB.categories.toLowerCase())*-1 
+            );
+            
+         }
+        if(NomArtisteTrie == "Croissant"){
+            setCategorieTrie("Rien")
+            var pieceMusicales=pieces;
+            pieceMusicales = pieceMusicales.sort((pieceA, pieceB) => 
+                pieceA.artiste.toLowerCase().localeCompare(pieceB.artiste.toLowerCase()) 
+                );
+                setNomArtisteTrie("rien");
+           
+        }
+        else if (NomArtisteTrie == "Decroissant") {   
+            
+            setCategorieTrie("Rien")
+            var pieceMusicales=pieces;
+            pieceMusicales = (pieceMusicales.sort((pieceA, pieceB) => 
+                pieceA.artiste.toLowerCase().localeCompare(pieceB.artiste.toLowerCase())*-1 
+                ));
+                setNomArtisteTrie("rien");
+        }
+        if(PieceTrie == "Croissant"){
+            setCategorieTrie("Rien")
+            var pieceMusicales=pieces;
+            pieceMusicales = pieceMusicales.sort((pieceA, pieceB) => 
+                pieceA.titre.toLowerCase().localeCompare(pieceB.titre.toLowerCase()) 
+                );
+                setPieceTrie("Rien");
+        }
+        else if (PieceTrie == "Decroissant"){
+            setCategorieTrie("Rien")
+            var pieceMusicales=pieces;
+            pieceMusicales = pieces;
+            pieceMusicales= pieceMusicales.sort((pieceA, pieceB) => 
+                pieceA.titre.toLowerCase().localeCompare(pieceB.titre.toLowerCase())*-1 
+                );// sort une function lamda
+                setPieceTrie("Rien");
+        }
+        
         return (
-            <>
-                {categories.map((categorie) => {
-                    const piecesAssociees = pieces.filter((piece) => 
-                        piece.categories.indexOf(categorie) !== -1);
+            <> 
+                <Table className="table table-bordered">
+                    <thead>
+                        <tr>
+                            <th>Titre
+                                <Button variant="success" className="m-1" size="sm" onClick={() => setPieceTrie("Croissant")} ><FaAngleUp /></Button>
+                                <Button variant="success" className="m-1" size="sm" onClick={() => setPieceTrie("Decroissant")} ><FaAngleDown/></Button>
+                            </th>
+                            <th>Artiste
+                                <Button variant="success" className="m-1" size="sm" onClick={() => setNomArtisteTrie("Croissant")}><FaAngleUp /></Button>
+                                <Button variant="success" className="m-1" size="sm" onClick={() => setNomArtisteTrie("Decroissant")} ><FaAngleDown/></Button>
+                            </th>
+                            <th>Categorie
+                                <Button variant="success" className="m-1" size="sm" onClick={() => setCategorieTrie("Croissant")} ><FaAngleUp /></Button>
+                                <Button variant="success" className="m-1" size="sm" onClick={() => setCategorieTrie("Decroissant")} ><FaAngleDown/></Button>
+                            </th> 
+                            <th>
+                                Modifier
+                            </th>     
+                            <th>
+                                Suprimer
+                            </th>                        
+                        </tr>
+                    </thead>
+                    <tbody>
+
+                        {pieceMusicales.map(piece => {
+                            if(typeof (piece.categories)!="string"){
+                                    return(
+                                        <>  
+                                            <tr>  
+                                                <td>{piece.titre}</td>
+                                                <td>{piece.artiste}</td>
+                                                <td>
+                                                    {piece.categories.map(categorie =>{
+                                                        return(
+                                                            <>
+                                                                <p>{categorie}</p>
+                                                            </>
+                                                        );
+                                                    })}
+                                                </td>
+                                                <td>
+                                                <Link to={`/modifier/${piece._id}`}>
+                                                    <Button variant="success" className="m-1" size="sm" >Modifier</Button>
+                                                </Link>
+                                                </td>
+                                                <td>
+                                                <Link to={`/supprimer/${piece._id}`}>
+                                                    <Button variant="danger" className="m-1" size="sm" >Supprimer</Button>
+                                                </Link>
+                                                </td>
+                                            </tr>
+                                        </>
+                                    ); 
+                                }                                      
+                                else{                 
+                                    return(
+                                        <>  
+                                            <tr>  
+                                                <td>{piece.titre}</td>
+                                                <td>{piece.artiste}</td>
+                                                <td>{piece.categories}</td>
+                                                <td>
+                                                <Link to={`/modifier/${piece._id}`}>
+                                                    <Button variant="success" className="m-1" size="sm" >Modifier</Button>
+                                                </Link>
+                                                </td>
+                                                <td>
+                                                <Link to={`/supprimer/${piece._id}`}>
+                                                    <Button variant="danger" className="m-1" size="sm" >Supprimer</Button>
+                                                </Link>
+                                                </td>
+                                            </tr>
+                                        </>
+                                    );
+                                } 
+                            } 
                         
-                    return (
-                        <div key={categorie}>
-                            <h4>{categorie}</h4>
-                            <ul>
-                                {
-                                    piecesAssociees.map(piece => 
-                                    <li key={piece._id}>{piece.titre} - {piece.artiste}
-                                        <Link to={`/modifier/${piece._id}`}>
-                                            <Button variant="success" className="m-1" size="sm" >Modifier</Button>
-                                        </Link>
-                                        <Link to={`/supprimer/${piece._id}`}>
-                                            <Button variant="danger" className="m-1" size="sm" >Supprimer</Button>
-                                        </Link>                                        
-                                    </li>)
-                                }
-                            </ul>
-                        </div>
-                    )
-                })}
+                        )}  
+                    </tbody>
+                </Table>
             </>
         );
     }
@@ -49,5 +196,6 @@ function ListePiecesAdmin({ pieces }) {
         return <Alert variant={"info"} >Il n'y a pas de pièces dans le répertoire.</Alert>;
     }
 }
+
 
 export default ListePiecesAdmin;
